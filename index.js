56e9abb005c6e04088e432a33de40fbc93cf15c2
carlos iglesias <>
2019-10-11 09:48:30 +0200

    pendint tests

--- index.js
+++ index.js
@@ -1,50 +1,96 @@
-function GoL(universe, step) {
-
-    return
-
-
-}
-
-
-
-
-function Universe(size = 10) {
-    let values;
-    let previousValues;
-
-    const isAlive = function(previousValues, i, j)  {
-        return 1;
-    } 
-
-    const obj = {
-        initValues: function () {
-            const fillEmpty = function () {
-                const createFalsyArray = (size) => Array.from(new Array(size), () => 0)
-                return Array.from(new Array(size), () => createFalsyArray(size))
-            }
-            values = fillEmpty();
-            values[0][0] = 1;
-            values[0][1] = 1
-        },
-
-        next: function () {
-            previousValues = values;
-            for (let i = 0; i < values.length; i++) {
-                let fila = values[i]
-                for (let j = 0; j < fila.length; j++) {
-                    console.log(`value: ${fila[j]}`)
-                    values[i][j] = isAlive(i,j)
-                }
-            }
-            console.log('done')
+function threeTrues(iterable) {
+    let count = 0;
+    for (const element of iterable) {
+        if ((element)) {
+            ++count;
         }
     }
+    return count === 3;
+}
 
-    return obj;
+function twoOrThreeTrues(iterable) {
+    let count = 0;
+    for (const element of iterable) {
+        if ((element)) {
+            ++count;
+        }
+    }
+    return count === 2 || count === 3;
 }
 
 
-const myUniverse = Universe();
-myUniverse.initValues();
-myUniverse.next();
-myUniverse.next();
+function* GameOfLife(board) {
+    let current = new Board(board);
+    const getter = container => (x, y) => container[y][x]
+    let count = 0
+    while (count++ <= 10) {
+        const boardContent = current.data;
+        let getterTmp = getter(boardContent);
+        let nextBoard = new Array();
+        for (let y = 0; y < boardContent.length; y++) {
+            let lineNextBoard = []
+            nextBoard.push(lineNextBoard)
+            for (let x = 0; x < boardContent[0].length; x++) {
+                    lineNextBoard.push(
+                        (getterTmp(x, y)) ? (+threeTrues(current.neighbour(x, y))) : (+twoOrThreeTrues(current.neighbour(x, y)))
+                    )                                     
+            }
+        }
+        yield nextBoard;
+        current = new Board(nextBoard);
+    }
+
+}
+
+
+
+function Board(data) {
+    this.data = data;
+    this.height = data.length;
+    this.width = data[0].length;
+    const get = (x, y) => this.data[y][x]
+
+    this.line = function* (x, y, center = true) {
+        if (x > 0) yield get(x - 1, y)
+        if (center) yield get(x, y)
+        if (x < this.width) yield get(x + 1, y)
+    }
+
+    this.neighbour = function* (x, y) {
+        if (y) yield* this.line(x, y - 1);
+        yield* this.line(x, y, false);
+        if (y + 1 < this.height) yield* this.line(x, y + 1)
+    }
+
+}
+
+
+const a = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+const b = [
+    [1, 2, 3, 0, 0, 0, 0, 0, 0, 0],
+    [8, 0, 4, 0, 0, 0, 0, 0, 7, 0],
+    [7, 6, 5, 1, 0, 0, 0, 1, 1, 1],
+    [0, 1, 1, 1, 0, 0, 0, 1, 9, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+const gol =  GameOfLife(a);
+const y = [...gol];
+console.log(y)
+
+// const t = new Board(b);
+// const it1 = t.neighbour(8, 5)
+// const itA1 = [...it1];
+// console.log(itA1)
+
+// const a2 = t.neighbour(8,3)
+
+// a2;
